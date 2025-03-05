d8deff370b1151b6bc560628bbb655c3d684d98e
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-03-05 22:39:38 +0530

    feat: Added auth context to store registered & logged in user

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,30 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
+import { AuthProvider, userAuth } from "../contexts/AuthContext";
+import { supabase } from "../lib/supabase";
 
 const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+};
+
+const MainLayout = () => {
+  const { setAuth } = userAuth();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("Session user: ", session?.user);
+
+      if (session) {
+      } else {
+      }
+    });
+  });
+
   return (
     <Stack
       screenOptions={{
