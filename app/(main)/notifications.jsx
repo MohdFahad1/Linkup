aeff8f7570f2a9d8297336d6fae7b67625058919
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-03-06 23:01:34 +0530

    feat: Added header section on home page

---
+++ app/(main)/notifications.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Notificatoins = () => {
+  return (
+    <View>
+      <Text>Notificatoins</Text>
+    </View>
+  );
+};
+
+export default Notificatoins;
+
+const styles = StyleSheet.create({});
