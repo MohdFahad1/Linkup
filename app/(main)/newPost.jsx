fdae327c8503e833fe94c10ee405a167feb54291
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-03-08 16:25:31 +0530

    feat: Added create post page

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,14 +1,223 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useRef, useState } from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import Header from "../../components/Header";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Avatar from "../../components/Avatar";
+import { userAuth } from "../../contexts/AuthContext";
+import RichTextEditor from "../../components/RichTextEditor";
+import { useRouter } from "expo-router";
+import Icon from "../../assets/icons";
+import Button from "../../components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "react-native";
+import { getSupabaseFileUri } from "../../services/imageService";
+import { Video } from "expo-av";
 
 const NewPost = () => {
+  const { user } = userAuth();
+  const bodyRef = useRef("");
+  const editorRef = useRef(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(file);
+
+  const onPick = async (isImage) => {
+    let mediaConfig = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.7,
+    };
+    if (!isImage) {
+      mediaConfig = {
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: true,
+      };
+    }
+    let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
+
+  const isLocaleFile = (file) => {
+    if (!file) return null;
+    if (typeof file == "object") return true;
+
+    return false;
+  };
+  const getFileType = (file) => {
+    if (!file) return null;
+
+    if (isLocaleFile) {
+      return file.type;
+    }
+
+    // check image or video for remote file
+    if (file.includes("postImage")) {
+      return "image";
+    }
+
+    return "video";
+  };
+
+  const getFileUri = (file) => {
+    if (!file) return null;
+    if (isLocaleFile(file)) {
+      return file.uri;
+    }
+
+    return getSupabaseFileUri(file)?.uri;
+  };
+
+  const onSubmit = async () => {};
+
   return (
-    <View>
-      <Text>NewPost</Text>
-    </View>
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        <Header title="Create Post" />
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
+          {/* avatar */}
+          <View style={styles.header}>
+            <Avatar
+              uri={user?.image}
+              size={hp(6.5)}
+              rounded={theme.radius.xl}
+            />
+            <View style={{ gap: 2 }}>
+              <Text style={styles.username}>{user && user.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
+            </View>
+          </View>
+
+          <View style={styles.textEditor}>
+            <RichTextEditor
+              editorRef={editorRef}
+              onChange={(body) => (bodyRef.current = body)}
+            />
+          </View>
+
+          {file && (
+            <View style={styles.file}>
+              {getFileType(file) == "video" ? (
+                <Video
+                  style={{ flex: 1 }}
+                  source={{
+                    uri: getFileUri(file),
+                  }}
+                  useNativeControls
+                  resizeMode="cover"
+                  isLooping
+                />
+              ) : (
+                <Image
+                  source={{ uri: getFileUri(file) }}
+                  resizeMethod="cover"
+                  style={{ flex: 1 }}
+                />
+              )}
+              <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                <Icon name="delete" size={20} color="white" />
+              </Pressable>
+            </View>
+          )}
+
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Add to your post</Text>
+            <View style={styles.medaiIcons}>
+              <TouchableOpacity onPress={() => onPick(true)}>
+                <Icon name="image" size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPick(false)}>
+                <Icon name="video" size={33} color={theme.colors.dark} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+        <Button
+          buttonStyle={{ height: hp(6.2) }}
+          loading={loading}
+          title="Post"
+          hasShadow={false}
+          onPress={onSubmit}
+        />
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default NewPost;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  avatar: {
+    height: hp(6.5),
+    width: wp(6.5),
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+  },
+  username: {
+    fontSize: hp(2.2),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textLight,
+  },
+  publicText: {
+    fontSize: hp(1.7),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textLight,
+  },
+  media: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1.5,
+    paddingHorizontal: 18,
+    paddingVertical: 10,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+  },
+  medaiIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  file: {
+    height: hp(30),
+    width: "100%",
+    borderRadius: theme.radius.xl,
+    overflow: "hidden",
+    borderCurve: "continuous",
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    padding: 7,
+    borderRadius: 50,
+    backgroundColor: "rgba(255,0,0,0.6)",
+  },
+});
