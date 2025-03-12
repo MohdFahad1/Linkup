0e88b22c1c7293e3202a38edc869560ae33c7dc4
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-03-12 22:01:47 +0530

    fix: fetching & displaying users own post in profile page

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,12 +1,13 @@
 import {
   Alert,
+  FlatList,
   Pressable,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import { supabase } from "../../lib/supabase";
 import { userAuth } from "../../contexts/AuthContext";
@@ -16,10 +17,16 @@ import { hp, wp } from "../../helpers/common";
 import Icon from "../../assets/icons";
 import { theme } from "../../constants/theme";
 import Avatar from "../../components/Avatar";
+import { fetchPosts } from "../../services/postService";
+import PostCard from "../../components/PostCard";
+import Loading from "../../components/Loading";
 
+var limit = 0;
 const Profile = () => {
   const { user, setAuth } = userAuth();
   const router = useRouter();
+  const [hasMore, setHasMore] = useState(true);
+  const [posts, setPosts] = useState([]);
 
   const onLogout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -44,9 +51,49 @@ const Profile = () => {
     ]);
   };
 
+  const getPosts = async () => {
+    if (!hasMore) return null;
+    limit = limit + 4;
+    console.log("Fetchig posts with limit: ", limit);
+
+    let res = await fetchPosts(limit, user.id);
+    if (res.success) {
+      if (posts.length == res.data.length) setHasMore(false);
+      setPosts(res.data);
+    }
+  };
+
   return (
     <ScreenWrapper bg="white">
-      <UserHeader user={user} router={router} handleLogout={handleLogout} />
+      <FlatList
+        data={posts}
+        ListHeaderComponent={
+          <UserHeader user={user} router={router} handleLogout={handleLogout} />
+        }
+        ListHeaderComponentStyle={{ marginBottom: 30 }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listStyle}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <PostCard item={item} currentUser={user} router={router} />
+        )}
+        onEndReached={() => {
+          getPosts();
+          console.log("Got to the end");
+        }}
+        onEndReachedThreshold={0}
+        ListFooterComponent={
+          hasMore ? (
+            <View style={{ marginVertical: posts.length == 0 ? 100 : 30 }}>
+              <Loading />
+            </View>
+          ) : (
+            <View style={{ marginVertical: 30 }}>
+              <Text style={styles.noPosts}>No more posts</Text>
+            </View>
+          )
+        }
+      />
     </ScreenWrapper>
   );
 };
